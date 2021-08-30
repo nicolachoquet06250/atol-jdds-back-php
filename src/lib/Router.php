af894ed2c401d6940fd166c28022e33489fb07e7
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-08-30 08:34:18 +0200

    Update Router.php

--- src/lib/Router.php
+++ src/lib/Router.php
@@ -36,6 +36,7 @@ class Router
     public function run() {
         dump("{$this->constants['baseUrl']}/{$_GET['q']}", $_SERVER['REQUEST_METHOD'], $_SERVER['HTTP_HOST'], $_SERVER['REMOTE_HOST']);
         dump(static::getRouter()->match("{$this->constants['baseUrl']}/{$_GET['q']}", $_SERVER['REQUEST_METHOD']));
+        dump(static::getRouter()->getRoutes());
         // static::getRouter()->match("{$this->constants['baseUrl']}/{$_GET['q']}", $_SERVER['REQUEST_METHOD'])['target']();
     }
 }
